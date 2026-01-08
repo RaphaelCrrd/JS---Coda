075a3a02d7af3f93dcaba29eb6d1c2562cce2482
RaphaelCrrd <ymeray.raphael@outlook.com>
2026-01-08 11:42:00 +0100

    TP1 Partie 2

--- TP1/script.js
+++ TP1/script.js
@@ -1,6 +1,11 @@
+// Partie 1
 const nomClasse = "B1-A";
 let nombreEleves = 28;
 let estOuverte = true;
 console.log("Le nom de la classe est : ", nomClasse);
 console.log("Le nombre d'eleves dans la classe est : ", nombreEleves);
 console.log("La classe est ouverte : ", estOuverte);
+
+// Partie 2
+const eleve = { name : "Raphael", noteMaths : 16, noteFrancais : 12};
+console.log("Le prénom de l'éleve est ", eleve.name);

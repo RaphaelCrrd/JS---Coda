aff0cd94b46e28630d14690cbfba1128b91dafab
RaphaelCrrd <ymeray.raphael@outlook.com>
2026-01-08 11:33:22 +0100

    TP1 Partie 1

---
+++ TP1/script.js
@@ -0,0 +1,6 @@
+const nomClasse = "B1-A";
+let nombreEleves = 28;
+let estOuverte = true;
+console.log("Le nom de la classe est : ", nomClasse);
+console.log("Le nombre d'eleves dans la classe est : ", nombreEleves);
+console.log("La classe est ouverte : ", estOuverte);
---
+++ script.js
@@ -0,0 +1,9 @@
+let count = 1;
+console.log(count);
+count = 2;
+console.log(count);
+
+const user = { name: "Alice" };
+console.log(user);
+user.name = "Bob";
+console.log(user);

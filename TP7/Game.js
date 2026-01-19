72b4ddd63c568a565d75eb6b6138bced412f38b0
RaphaelCrrd <ymeray.raphael@outlook.com>
2026-01-19 10:39:11 +0100

    TP7

---
+++ TP7/Game.js
@@ -0,0 +1,107 @@
+// Exemple de message recu par le backend, à utiliser pour vos tests :
+const backendData = {
+   "isRunning":true,
+   "isOver":false,
+   "timer":190.6000000000091,
+   "players":{
+      "3cd71bbb-6a6b-4d4e-80e3-107130328a27":{
+         "name":"blabla",
+         "skinPath":"../TP6/assets/3.png",
+         "position":[
+            0.5600000000000003,
+            0.17999999999999977
+         ],
+         "lvl":1,
+         "hp":100,
+         "maxHp":100,
+         "hpRegenRate":10,
+         "speed":0.2,
+         "direction":3,
+         "isAttacking":false,
+         "isWalking":false,
+         "isDying":false,
+         "attackCooldown":1,
+         "currentAttackCooldown":0
+      },
+      "28ead291-fcea-4b41-a596-d3c876c49a53":{
+         "name":"bloublou",
+         "skinPath":"../TP6/assets/4.png",
+         "position":[
+            0.44,
+            0.19
+         ],
+         "lvl":1,
+         "hp":100,
+         "maxHp":100,
+         "hpRegenRate":10,
+         "speed":0.2,
+         "direction":0,
+         "isAttacking":false,
+         "isWalking":false,
+         "isDying":false,
+         "attackCooldown":1,
+         "currentAttackCooldown":0
+      }
+   }
+};
+
+class Game {
+   constructor() {
+      this.isRunning = false;
+      this.isOver = false;
+      this.timer = 0;
+      this.players = {};
+   }
+
+
+   update(gameStateFromServer) {
+      this.isRunning = gameStateFromServer.isRunning;
+      this.isOver = gameStateFromServer.isOver;
+      this.timer = gameStateFromServer.timer;
+
+      const serverPlayerIds = Object.keys(gameStateFromServer.players);  // Recupe tous les UUID sous forme de liste
+
+      serverPlayerIds.forEach(id => {
+         const dataFromServer = gameStateFromServer.players[id];
+
+         if (this.players[id]) {
+            this.players[id].update(dataFromServer);
+         }
+         else {
+            const newPlayer = new Player(
+               dataFromServer.name,
+               dataFromServer.skinPath,
+               dataFromServer.position[0],
+               dataFromServer.position[1]
+            );
+
+            this.players[id] = newPlayer;
+            this.players[id].update(dataFromServer);
+
+            console.log(`Nouveau joueur ajouté : ${dataFromServer.name} (ID: ${id})`);
+         }
+      });
+
+      const localPlayersIds = Object.keys(this.players);
+
+      localPlayersIds.forEach(id => {
+         if (!gameStateFromServer.players[id]) {
+            console.log(`Le joueur ${this.players[id].name} a quitté la partie.`)
+            delete this.players[id];
+         }
+      });
+   }
+}
+
+const gameTest = new Game();
+
+console.log("Réception");
+gameTest.update(backendData);
+console.log("Joueurs crées dans le front :", Object.keys(gameTest.players));
+
+console.log("Changement de PV");
+let nextTickData = JSON.parse(JSON.stringify(backendData));
+const player1Id = "3cd71bbb-6a6b-4d4e-80e3-107130328a27";
+nextTickData.players[player1Id].hp = 80;
+gameTest.update(nextTickData);
+console.log(`PV de blabla : ${gameTest.players[player1Id].hp} (normalement 80)`);
